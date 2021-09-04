00c6cd4846b67dd45c2ebb5272eb82e5bab39914
Anton Brekhov <anton.brekhov@rsc-tech.ru>
2021-09-04 09:34:35 +0300

    ft add mocks

--- back/main.go
+++ back/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -8,18 +9,31 @@ import (
 	echo "github.com/labstack/echo/v4"
 )
 
+type Response struct {
+	Msg string `json:"msg"`
+}
+
+type Datatsets struct {
+	Datatsets []Datatset `json:"datasets"`
+}
+
+type Datatset struct {
+	Name string `json:"name"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/check", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.POST("/upload", Upload)
-
+	e.POST("/data", Upload)
+	e.PUT("/data", Replace)
+	e.GET("/datasets", GetDatasets)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func Upload(c echo.Context) error {
-	name := c.FormValue("name")
+	dataset_name := c.FormValue("name")
 	// Get avatar
 	sef, err := c.FormFile("social_economy_forecast")
 	if err != nil {
@@ -45,5 +59,48 @@ func Upload(c echo.Context) error {
 		return err
 	}
 
-	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
+	// func Process File
+
+	return c.JSON(http.StatusOK, &Response{Msg: dataset_name + " uploaded"})
+}
+
+func Replace(c echo.Context) error {
+	dataset_name := c.FormValue("name")
+	// Get avatar
+	sef, err := c.FormFile("social_economy_forecast")
+	if err != nil {
+		return err
+	}
+
+	// Source
+	src, err := sef.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(sef.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	// func Replace in DB with new content
+
+	return c.JSON(http.StatusOK, &Response{Msg: dataset_name + " replaced"})
+}
+
+func GetDatasets(c echo.Context) error {
+	var datatsets Datatsets
+	for i := 0; i < 10; i++ {
+		datatsets.Datatsets = append(datatsets.Datatsets, Datatset{Name: fmt.Sprintf("dataset%d", i)})
+	}
+	c.JSON(http.StatusOK, datatsets)
+	return nil
 }
